ee44ac5a8e8d9fe20109f2490cfeb1f1d15035cb
mt-yasin <96203359+mt-shamrat@users.noreply.github.com>
2021-12-30 18:31:07 +0600

    init

---
+++ lib/app/controllers/HomeController.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  @override
+  void onInit() {
+    super.onInit();
+
+    // get monthly overview
+    // get recent 5 records
+  }
+}
