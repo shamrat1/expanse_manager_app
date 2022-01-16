f72bfea0aba370a83a68ab5649834cf94ceafb31
mt-yasin <96203359+mt-shamrat@users.noreply.github.com>
2022-01-16 12:39:52 +0600

    Transaction service added

--- lib/app/services/TransactionService.dart
+++ lib/app/services/TransactionService.dart
@@ -25,6 +25,42 @@ class TransactionService {
   Future<Map<String, String>> _getHeader() async {
     var box = await Hive.openBox(hiveBox);
     var token = box.get(authToken);
-    return {"accept": "application/json", "Authorization": "Bearer " + token};
+    return {
+      "accept": "application/json",
+      "Authorization": "Bearer " + token,
+    };
+  }
+
+  Future<http.Response> postTransaction(
+    Map<String, dynamic> data, {
+    bool income = false,
+  }) async {
+    var url = rootURL + (income ? "incomes" : "expenses");
+    var response = await http.post(
+      Uri.parse(url),
+      headers: await _getHeader(),
+      body: data,
+    );
+    return response;
+  }
+
+  Future<http.Response> putTransaction(Map<String, dynamic> data, int id,
+      {bool income = false}) async {
+    var url = rootURL + (income ? "incomes" : "expenses") + "/$id";
+    var response = await http.post(
+      Uri.parse(url),
+      headers: await _getHeader(),
+      body: data,
+    );
+    return response;
+  }
+
+  Future<http.Response> deleteTransaction(int id, {bool income = false}) async {
+    var url = rootURL + (income ? "incomes" : "expenses") + "/$id";
+    var response = await http.delete(
+      Uri.parse(url),
+      headers: await _getHeader(),
+    );
+    return response;
   }
 }
