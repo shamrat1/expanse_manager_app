b97e504a0ba19466404714ff798497c9e5b1d4fd
shamrat1 <yshamrat@gmail.com>
2022-02-01 10:57:51 +0600

    Theme Changed

--- lib/app/controllers/HomeController.dart
+++ lib/app/controllers/HomeController.dart
@@ -1,8 +1,13 @@
+import 'package:expanse_manager/app/helpers.dart/Globals.dart';
 import 'package:expanse_manager/app/models/HomeResponse.dart';
 import 'package:expanse_manager/app/models/TransactionResponse.dart';
 import 'package:expanse_manager/app/services/TransactionService.dart';
+import 'package:expanse_manager/app/themes/textTheme.dart';
 import 'package:expanse_manager/views/screens/create_transaction.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
 
 class HomeController extends GetxController {
   RxBool loading = false.obs;
@@ -22,7 +27,40 @@ class HomeController extends GetxController {
     loading(false);
   }
 
-  void gotoNewTransactionPage(String type) {
-    Get.to(() => CreateTransaction(), arguments: type);
+  void gotoNewTransactionPage(String type) async {
+    var box = await Hive.openBox(hiveBox);
+    var themeString = box.get(themeKey);
+    print(themeString);
+    if (themeString != null) {
+      if (themeString == 'dark') {
+        box.put(themeKey, 'light');
+        Get.changeTheme(FlexThemeData.light(
+          scheme: FlexScheme.rosewood,
+          textTheme: myBaseTextTheme,
+        ));
+      } else {
+        box.put(themeKey, 'dark');
+        Get.changeTheme(FlexThemeData.dark(
+          scheme: FlexScheme.rosewood,
+          textTheme: myBaseTextTheme,
+        ));
+      }
+    } else {
+      if (MediaQuery.of(Get.context!).platformBrightness == Brightness.dark) {
+        box.put(themeKey, 'light');
+        Get.changeTheme(FlexThemeData.light(
+          scheme: FlexScheme.rosewood,
+          textTheme: myBaseTextTheme,
+        ));
+      } else {
+        box.put(themeKey, 'dark');
+        Get.changeTheme(FlexThemeData.dark(
+          scheme: FlexScheme.rosewood,
+          textTheme: myBaseTextTheme,
+        ));
+      }
+    }
+
+    // Get.to(() => CreateTransaction(), arguments: type);
   }
 }
